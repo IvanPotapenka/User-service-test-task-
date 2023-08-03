10ffef6f5fb02fac847c320b57b097ea44e02fcc
PIY <puivan617@gmail.com>
2023-08-04 01:05:22 +0300

    fix method update

--- model/src/main/java/by/potapenko/model/dto/RoleDto.java
+++ model/src/main/java/by/potapenko/model/dto/RoleDto.java
@@ -2,10 +2,12 @@ package by.potapenko.model.dto;
 
 import by.potapenko.model.enam.Role;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class RoleDto {
